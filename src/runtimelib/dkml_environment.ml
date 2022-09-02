648a1e34be7ac99fb46e1c25eef5f7ce66fb0ceb
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-09-02 06:48:01 -0700

    Add MINGW_ vars
    diskuv/dkml-installer-ocaml#7

--- src/runtimelib/dkml_environment.ml
+++ src/runtimelib/dkml_environment.ml
@@ -38,10 +38,15 @@ let set_msys2_entries ~minimize_sideeffects target_platform_name =
   Lazy.force get_msys2_dir_opt >>= function
   | None -> R.ok ()
   | Some msys2_dir ->
-      (* See https://www.msys2.org/docs/environments/ for the magic values.
+      (* See https://github.com/msys2/MSYS2-packages/blob/1ff9c79a6b6b71492c4824f9888a15314b85f5fa/filesystem/msystem and
+         https://www.msys2.org/docs/environments/ for the magic values.
 
           1. MSYSTEM = MINGW32 or CLANG64
-          2. MSYSTEM_CARCH, MSYSTEM_CHOST, MSYSTEM_PREFIX for 64-bit MSYS
+          2. MSYSTEM_CARCH, MSYSTEM_CHOST, MSYSTEM_PREFIX
+          3. MINGW_CHOST, MINGW_PREFIX, MINGW_PACKAGE_PREFIX
+
+          32 bit notes
+          ------------
 
           There is no 32-bit MSYS2 tooling (well, 32-bit was deprecated), but you don't need 32-bit
           MSYS2 binaries; just a 32-bit (cross-)compiler.
@@ -51,34 +56,68 @@ let set_msys2_entries ~minimize_sideeffects target_platform_name =
           Confer: https://issuemode.com/issues/msys2/MINGW-packages/18837088
       *)
       (match target_platform_name with
-      | "windows_x86" -> R.ok ("MINGW32", "i686", "i686-w64-mingw32", "mingw32")
+      | "windows_x86" ->
+          R.ok
+            ( "MINGW32",
+              "i686",
+              "i686-w64-mingw32",
+              "mingw32",
+              "i686-w64-mingw32",
+              "mingw32",
+              "mingw-w64-i686" )
       | "windows_x86_64" ->
-          R.ok ("CLANG64", "x86_64", "x86_64-w64-mingw32", "clang64")
+          R.ok
+            ( "CLANG64",
+              "x86_64",
+              "x86_64-w64-mingw32",
+              "clang64",
+              "x86_64-w64-mingw32",
+              "clang64",
+              "mingw-w64-clang-x86_64" )
       | "windows_arm64" ->
-          R.ok ("CLANGARM64", "aarch64", "aarch64-w64-mingw32", "clangarm64")
+          R.ok
+            ( "CLANGARM64",
+              "aarch64",
+              "aarch64-w64-mingw32",
+              "clangarm64",
+              "aarch64-w64-mingw32",
+              "clangarm64",
+              "mingw-w64-clang-aarch64" )
       | _ ->
           R.error_msg @@ "The target platform name '" ^ target_platform_name
           ^ "' is not a supported Windows platform")
-      >>= fun (msystem, carch, chost, prefix) ->
+      >>= fun ( msystem,
+                msystem_carch,
+                msystem_chost,
+                msystem_prefix,
+                mingw_chost,
+                mingw_prefix,
+                mingw_package_prefix ) ->
       OS.Env.set_var "MSYSTEM" (Some msystem) >>= fun () ->
-      OS.Env.set_var "MSYSTEM_CARCH" (Some carch) >>= fun () ->
-      OS.Env.set_var "MSYSTEM_CHOST" (Some chost) >>= fun () ->
-      OS.Env.set_var "MSYSTEM_PREFIX" (Some ("/" ^ prefix)) >>= fun () ->
+      OS.Env.set_var "MSYSTEM_CARCH" (Some msystem_carch) >>= fun () ->
+      OS.Env.set_var "MSYSTEM_CHOST" (Some msystem_chost) >>= fun () ->
+      OS.Env.set_var "MSYSTEM_PREFIX" (Some ("/" ^ msystem_prefix))
+      >>= fun () ->
+      OS.Env.set_var "MINGW_CHOST" (Some mingw_chost) >>= fun () ->
+      OS.Env.set_var "MINGW_PREFIX" (Some ("/" ^ mingw_prefix)) >>= fun () ->
+      OS.Env.set_var "MINGW_PACKAGE_PREFIX" (Some mingw_package_prefix)
+      >>= fun () ->
       (* 2. Fix the MSYS2 ambiguity problem described at https://github.com/msys2/MSYS2-packages/issues/2316.
-          Our error is running:
-            cl -nologo -O2 -Gy- -MD -Feocamlrun.exe prims.obj libcamlrun.lib advapi32.lib ws2_32.lib version.lib /link /subsystem:console /ENTRY:wmainCRTStartup
-          would warn
-            cl : Command line warning D9002 : ignoring unknown option '/subsystem:console'
-            cl : Command line warning D9002 : ignoring unknown option '/ENTRY:wmainCRTStartup'
-          because the slashes (/) could mean Windows paths or Windows options. We force the latter.
+         Our error is running:
+           cl -nologo -O2 -Gy- -MD -Feocamlrun.exe prims.obj libcamlrun.lib advapi32.lib ws2_32.lib version.lib /link /subsystem:console /ENTRY:wmainCRTStartup
+         would warn
+           cl : Command line warning D9002 : ignoring unknown option '/subsystem:console'
+           cl : Command line warning D9002 : ignoring unknown option '/ENTRY:wmainCRTStartup'
+         because the slashes (/) could mean Windows paths or Windows options. We force the latter.
 
-          This is described in Automatic Unix ⟶ Windows Path Conversion
-          at https://www.msys2.org/docs/filesystem-paths/
+         This is described in Automatic Unix ⟶ Windows Path Conversion
+         at https://www.msys2.org/docs/filesystem-paths/
       *)
       OS.Env.set_var "MSYS2_ARG_CONV_EXCL" (Some "*") >>= fun () ->
       (* 3. Remove MSYS2 entries, if any, from PATH
             _unless_ we are minimizing side-effects *)
-      (if minimize_sideeffects then Ok () else prune_path_of_msys2 prefix)
+      (if minimize_sideeffects then Ok ()
+      else prune_path_of_msys2 msystem_prefix)
       >>= fun () ->
       (* 4. Add MSYS2 <prefix>/bin and /usr/bin to front of PATH
             _unless_ we are minimizing side-effects. *)
@@ -87,7 +126,7 @@ let set_msys2_entries ~minimize_sideeffects target_platform_name =
         OS.Env.req_var "PATH" >>= fun path ->
         OS.Env.set_var "PATH"
           (Some
-             (Fpath.(msys2_dir / prefix / "bin" |> to_string)
+             (Fpath.(msys2_dir / msystem_prefix / "bin" |> to_string)
              ^ ";"
              ^ Fpath.(msys2_dir / "usr" / "bin" |> to_string)
              ^ ";" ^ path))
