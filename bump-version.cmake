127a75807fa42c10ea3bae418b521dba34b78901
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-06-30 22:30:02 -0700

    Rename DkMLReleaseParticipant to DkMLBumpVersionParticipant

--- bump-version.cmake
+++ bump-version.cmake
@@ -1,17 +1,17 @@
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-apps -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLReleaseParticipant.cmake -P bump-version.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-runtime-apps -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.0 -D DKML_RELEASE_PARTICIPANT_MODULE=../../../pkg/bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
 
 if(NOT DKML_RELEASE_PARTICIPANT_MODULE)
-    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLReleaseParticipant.cmake")
+    message(FATAL_ERROR "Missing -D DKML_RELEASE_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
 endif()
 include(${DKML_RELEASE_PARTICIPANT_MODULE})
 
-DkMLReleaseParticipant_OpamReplace(dkml-apps.opam)
-DkMLReleaseParticipant_OpamReplace(dkml-exe-lib.opam)
-DkMLReleaseParticipant_OpamReplace(dkml-exe.opam)
-DkMLReleaseParticipant_OpamReplace(dkml-runtimelib.opam)
-DkMLReleaseParticipant_OpamReplace(dkml-runtimescripts.opam)
-DkMLReleaseParticipant_OpamReplace(opam-dkml.opam)
-DkMLReleaseParticipant_OpamReplace(with-dkml.opam)
-DkMLReleaseParticipant_DuneProjectReplace(dune-project)
-DkMLReleaseParticipant_GitAddAndCommit()
+DkMLBumpVersionParticipant_OpamReplace(dkml-apps.opam)
+DkMLBumpVersionParticipant_OpamReplace(dkml-exe-lib.opam)
+DkMLBumpVersionParticipant_OpamReplace(dkml-exe.opam)
+DkMLBumpVersionParticipant_OpamReplace(dkml-runtimelib.opam)
+DkMLBumpVersionParticipant_OpamReplace(dkml-runtimescripts.opam)
+DkMLBumpVersionParticipant_OpamReplace(opam-dkml.opam)
+DkMLBumpVersionParticipant_OpamReplace(with-dkml.opam)
+DkMLBumpVersionParticipant_DuneProjectReplace(dune-project)
+DkMLBumpVersionParticipant_GitAddAndCommit()
