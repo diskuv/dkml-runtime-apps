d5890120fc0efe7a0e98f80884790228e4fbad58
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2023-11-19 08:24:00 -0800

    Add dkml init --system (5/n)
    + Switch from dkml-compiler-env to dkml-compiler-src
    
    Part of https://github.com/diskuv/dkml-installer-ocaml/issues/77

--- src/runtimescripts/dkml_runtimescripts.ml
+++ src/runtimescripts/dkml_runtimescripts.ml
@@ -1,24 +1,32 @@
 open Bos
 
 let extract_dkml_scripts ~dkmlversion dir_fp =
-  let ( >>= ) = Result.bind in
-  List.fold_left
-    (fun acc filename ->
-      match acc with
-      | Ok _ ->
-          (* mkdir (parent filename) *)
-          Fpath.of_string filename >>= fun filename_fp ->
-          let target_fp = Fpath.(dir_fp // filename_fp) in
-          let target_dir_fp = Fpath.(parent target_fp) in
-          OS.Dir.create target_dir_fp |> ignore;
-          (* cp script filename *)
-          let script_opt = Dkml_scripts.read filename in
-          Option.fold ~none:(Result.Ok ())
-            ~some:(fun script -> OS.File.write ~mode:0x755 target_fp script)
-            script_opt
-      | Error _ as err -> err)
-    (Result.Ok ()) Dkml_scripts.file_list
-  >>= fun () ->
+  let ( let* ) = Result.bind in
+  let file_list_helper file_list dest_fp =
+    List.fold_left
+      (fun acc filename ->
+        match acc with
+        | Ok _ ->
+            (* mkdir (parent filename) *)
+            let* filename_fp = Fpath.of_string filename in
+            let target_fp = Fpath.(dest_fp // filename_fp) in
+            let target_dir_fp = Fpath.(parent target_fp) in
+            OS.Dir.create target_dir_fp |> ignore;
+            (* cp script filename *)
+            let script_opt = Dkml_scripts.read filename in
+            Option.fold ~none:(Result.Ok ())
+              ~some:(fun script -> OS.File.write ~mode:0x755 target_fp script)
+              script_opt
+        | Error _ as err -> err)
+      (Result.Ok ()) file_list
+  in
+  (* extract everything from Dkml_scripts *)
+  let* () = file_list_helper Dkml_scripts.file_list dir_fp in
+  (* extract everything from Dkml_compiler_src into vendor/dkml-compiler *)
+  let* () =
+    file_list_helper Dkml_compiler_src.file_list
+      Fpath.(dir_fp / "vendor" / "dkml-compiler")
+  in
   (* create .dkmlroot from template.dkmlroot *)
   let path = "vendor/dkml-compiler/template.dkmlroot" in
   match Dkml_scripts.read path with
